6c5ba78c5ddd35e88c51fdee4d9219ddc90b9ee4
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-02-05 16:28:49 +0700

    Initial commit

---
+++ lib/App/throttle.pm
@@ -0,0 +1,16 @@
+package App::throttle;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Bandwidth limiting pipe
+
+=head1 SYNOPSIS
+
+See the included script L<throttle>.
+
+
+=head1 DESCRIPTION
+
+=head1 CUT
